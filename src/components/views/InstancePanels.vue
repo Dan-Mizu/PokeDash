4270bc99082f3e1d4dbdc0d09e9cfb8604bd385b
Dan <dan@danmizu.dev>
2023-11-02 11:38:24 -0700

    added sidebar to each panel

--- src/components/views/InstancePanels.vue
+++ src/components/views/InstancePanels.vue
@@ -7,6 +7,7 @@ const store = useStore();
 const panelStyleBase =
 	"bg-light-secondary dark:bg-dark-secondary px-2 py-1 mx-2 rounded-md flex flex-1 items-center justify-center";
 const panelStyle = [
+	"flex-[0.06] " + panelStyleBase,
 	"flex-[0.3] " + panelStyleBase,
 	"full:flex-[1] " + panelStyleBase,
 	"flex-[0.5] " + panelStyleBase,
@@ -32,23 +33,31 @@ const modalAddInstanceOpen = ref(false);
 	<div class="mt-5 w-[90%] flex justify-center">
 		<ul class="overflow-auto my-3 w-full scrollbar-hidden">
 			<li class="flex justify-center mb-3">
+				<!-- Sidebar Spacing -->
+				<div
+					:class="[
+						'truncate text-clip bg-transparent dark:bg-transparent',
+						panelStyle[0],
+					]"
+				></div>
+
 				<!-- Trainer Info -->
-				<div :class="['truncate text-clip', panelStyle[0]]">
+				<div :class="['truncate text-clip', panelStyle[1]]">
 					<h1 class="font-bold">Trainer</h1>
 				</div>
 
 				<!-- Team Info -->
-				<div :class="['truncate text-clip', panelStyle[1]]">
+				<div :class="['truncate text-clip', panelStyle[2]]">
 					<h1 class="font-bold">Team</h1>
 				</div>
 
 				<!-- Encounter Info -->
-				<div :class="['truncate text-clip', panelStyle[2]]">
+				<div :class="['truncate text-clip', panelStyle[3]]">
 					<h1 class="font-bold">Encounters</h1>
 				</div>
 
 				<!-- Stats -->
-				<div :class="['truncate text-clip', panelStyle[3]]">
+				<div :class="['truncate text-clip', panelStyle[4]]">
 					<h1 class="font-bold">Stats</h1>
 				</div>
 			</li>
