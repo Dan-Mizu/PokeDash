cbd401f1f1e1b5f772c8b99396dfa827b7623e17
U2EZNeko <nobody.cares@gmx.net>
2023-11-03 08:15:08 +0100

    Prettied up stats a bit.

--- src/components/presets/card/StatsCard.vue
+++ src/components/presets/card/StatsCard.vue
@@ -9,39 +9,41 @@ const props = defineProps<{
 <template>
 	<div :class="panelStyle">
 		<div class= "flex h-flex w-full p-3 grid">
-			<div class="bg-light-primary dark:bg-dark-primary rounded-md p-1 mb-2 text-s text-center my-2 py-2 px-3">
+			<div class="bg-light-primary dark:bg-dark-primary rounded-md pt-1 text-s text-center my-2 py-2 px-3">
 				<!-- Encounter Infos -->
 				<span class="flex justify-center items-center text-center mt-2 text-s">
-					<div class="text-left">Total Encounters: {{ data.totals.encounters }}</div>
-					<div class="text-left">Phase Encounters: {{ data.totals.phase_encounters }}</div>
-					<div class="text-left">Phase Highest IVSum: {{ data.totals.phase_highest_iv_sum }}</div>
+					<div class="text-left">Total Encounters:&emsp;<br> {{ data.totals.encounters }}</div>
+					<div class="text-left">Phase Encounters:&emsp;<br> {{ data.totals.phase_encounters }}</div>
 				</span>
 			</div>
 			<div class="bg-light-primary dark:bg-dark-primary rounded-md p-1 text-s text-center my-2 py-2 px-3">
-				<!-- Coords -->
+				<!-- Total Highest IVSum -->
 				<span class="flex justify-center items-center text-center mt-2 text-s">
-					<div class="text-left">Total Highest IV Pokemon: {{ data.totals.highest_iv_sum_pokemon }}</div>
-					<div class="text-left">Total Highest IVSum: {{ data.totals.highest_iv_sum }}</div>
+					<div class="text-left">Total Highest IV Pokemon:&emsp;<br> {{ data.totals.highest_iv_sum_pokemon }}</div>
+					<div class="text-left">Total Highest IVSum:&emsp;<br> {{ data.totals.highest_iv_sum }}</div>
 				</span>
 			</div>
 			<div class="bg-light-primary dark:bg-dark-primary rounded-md p-1 text-s text-center my-2 py-2 px-3">
+				<!-- Total Lowest IVSum -->
 				<span class="flex justify-center items-center text-center mt-2 text-s">
-					<div class="text-left">Total Lowest IV Pokemon: {{ data.totals.lowest_iv_sum_pokemon }}</div>
-					<div class="text-left">Total Lowest IVSums: {{ data.totals.lowest_iv_sum }}</div>
+					<div class="text-left">Total Lowest IV Pokemon:&emsp;<br> {{ data.totals.lowest_iv_sum_pokemon }}</div>
+					<div class="text-left">Total Lowest IVSums:&emsp;<br> {{ data.totals.lowest_iv_sum }}</div>
 				</span>
 			</div>
 			<div class="bg-light-primary dark:bg-dark-primary rounded-md p-1 text-s text-center my-2 py-2 px-3">
+				<!-- Phase Lowest IVSum -->
 				<span class="flex justify-center items-center text-center mt-2 text-s">
-					<div class="text-left">Phase Lowest IV Pokemon: {{ data.totals.phase_lowest_iv_sum_pokemon }}</div>
-					<div class="text-left">Phase Lowest IVSum: {{ data.totals.phase_lowest_iv_sum }}</div>
+					<div class="text-left">Phase Lowest IV Pokemon:&emsp;<br> {{ data.totals.phase_lowest_iv_sum_pokemon }}</div>
+					<div class="text-left">Phase Lowest IVSum:&emsp;<br> {{ data.totals.phase_lowest_iv_sum }}</div>
 				</span>
 			</div>
 			<div class="bg-light-primary dark:bg-dark-primary rounded-md p-1 text-s text-center my-2 py-2 px-3">
+				<!-- Shiny averages -->
 				<span class="flex justify-center items-center text-center mt-2 text-s">
-					<div class="text-left">Shiny Average: {{ data.totals.shiny_average }}</div>
-					<div class="text-left">Shiny Encounters: {{ data.totals.shiny_encounters }}</div>
-					<div class="text-left">Shortest Phase Encounters: {{ data.totals.shortest_phase_encounters }}</div>
-					<div class="text-left">Shortest Phase Pokemon: {{ data.totals.shortest_phase_pokemon }}</div>
+					<div class="text-left">Shiny Average:&emsp;<br> {{ data.totals.shiny_average }}</div>
+					<div class="text-left">Shiny Encounters:&emsp;<br> {{ data.totals.shiny_encounters }}</div>
+					<div class="text-left">Shortest Phase Encounters:&emsp;&emsp;<br> {{ data.totals.shortest_phase_encounters }}</div>
+					<div class="text-left">Shortest Phase Pokemon:&emsp;<br> {{ data.totals.shortest_phase_pokemon }}</div>
 				</span>
 			</div>
 		</div>
