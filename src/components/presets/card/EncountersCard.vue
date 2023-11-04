7f8cc844d9dff65406ae43f6c7515f39dff16d22
Dan <dan@danmizu.dev>
2023-11-03 18:50:56 -0700

    Reversed encounter list

--- src/components/presets/card/EncountersCard.vue
+++ src/components/presets/card/EncountersCard.vue
@@ -9,7 +9,9 @@ const props = defineProps<{
 
 // handle infinite scrolling of wild pokemon encounters
 const encounteredWildPokemonList = ref<HTMLElement>();
-const encounteredWildPokemonData = ref(props.encounterLogData.slice(0, 2));
+const encounteredWildPokemonData = ref(
+	props.encounterLogData.toReversed().slice(0, 2)
+);
 useInfiniteScroll(
 	encounteredWildPokemonList,
 	() => {
@@ -23,10 +25,12 @@ useInfiniteScroll(
 		// load more
 		encounteredWildPokemonData.value =
 			encounteredWildPokemonData.value.concat(
-				props.encounterLogData.slice(
-					encounteredWildPokemonData.value.length,
-					encounteredWildPokemonData.value.length + 3
-				)
+				props.encounterLogData
+					.toReversed()
+					.slice(
+						encounteredWildPokemonData.value.length,
+						encounteredWildPokemonData.value.length + 3
+					)
 			);
 	},
 	{ distance: 10 }
@@ -34,7 +38,9 @@ useInfiniteScroll(
 
 // handle infinite scrolling of shiny wild pokemon encounters
 const encounteredShinyWildPokemonList = ref<HTMLElement>();
-const encounteredShinyWildPokemonData = ref(props.shinyLogData.slice(0, 2));
+const encounteredShinyWildPokemonData = ref(
+	props.shinyLogData.toReversed().slice(0, 2)
+);
 useInfiniteScroll(
 	encounteredShinyWildPokemonList,
 	() => {
@@ -48,10 +54,12 @@ useInfiniteScroll(
 		// load more
 		encounteredShinyWildPokemonData.value =
 			encounteredShinyWildPokemonData.value.concat(
-				props.shinyLogData.slice(
-					encounteredShinyWildPokemonData.value.length,
-					encounteredShinyWildPokemonData.value.length + 3
-				)
+				props.shinyLogData
+					.toReversed()
+					.slice(
+						encounteredShinyWildPokemonData.value.length,
+						encounteredShinyWildPokemonData.value.length + 3
+					)
 			);
 	},
 	{ distance: 10 }
@@ -96,9 +104,7 @@ const encounterType = ref(0);
 			</div>
 
 			<!-- Encountered Wild Pokemon List -->
-			<div
-				class="flex flex-grow min-h-0 w-full justify-center"
-			>
+			<div class="flex flex-grow min-h-0 w-full justify-center">
 				<ul
 					v-if="encounterType === 0"
 					ref="encounteredWildPokemonList"
