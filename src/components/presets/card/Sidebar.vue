a11844c7e54f413065aef677fd4bfe6a5fbbd9c0
U2EZNeko <nobody.cares@gmx.net>
2023-11-05 13:20:58 +0100

    Added detailed stats modal and sidebar

--- src/components/presets/card/Sidebar.vue
+++ src/components/presets/card/Sidebar.vue
@@ -2,8 +2,20 @@
 const props = defineProps<{
 	panelStyle: string;
 }>();
+// Add Instance Modal handling
+const modalDetailedStatsOpen = ref(false);
 </script>
 
 <template>
-	<div :class="panelStyle"></div>
+	<div :class="panelStyle">
+		<div class="flex flex-col m-4">
+			<DetailedStatsButton />
+					<span v-if="props.allowInstanceManipulation">
+						<div class="mb-2">
+							<IconButton	@clickEvent="() => (modalDetailedStatsOpen = true)"	icon="i-material-symbols-add-box-rounded"/>
+						</div>
+					</span>
+			<DetailedStatsModal	:open="modalDetailedStatsOpen" @closeModal="modalDetailedStatsOpen = false"/>
+		</div>
+	</div>
 </template>
---
+++ src/components/presets/modals/DetailedStatsModal.vue
@@ -0,0 +1,134 @@
+<script setup lang="ts">
+// get state
+import useStore from "~/stores";
+const store = useStore();
+
+const props = defineProps<{
+	open: boolean;
+}>();
+
+// get input
+const inputComponent = ref();
+
+// submit
+const submit = () => {
+	// get input
+	const input = inputComponent.value.input;
+	data = IStats;
+	emuData = IEmuData;
+};
+</script>
+
+<template>
+	<Modal :open="open" @closeModal="$emit('closeModal')">
+	<div :class="panelStyle">
+				<div class="grid gap-2 w-full p-2 text-xs">
+					<!-- Encounter Infos -->
+					<div
+						class="bg-light-primary dark:bg-dark-primary rounded-md grid grid-flow-col p-2 gap-2"
+					>
+						<div class="grid grid-flow-row text-left">
+							<span>Total Encounters</span
+							><span class="font-bold text-sm self-end text-light-text-placeholder dark:text-dark-text-placeholder">{{
+								data.totals.encounters
+							}}</span>
+						</div>
+						<div class="grid grid-flow-row text-right">
+							<span>Phase Encounters</span>
+							<span class="font-bold text-sm self-end text-light-text-placeholder dark:text-dark-text-placeholder">{{
+								data.totals.phase_encounters
+							}}</span>
+						</div>
+					</div>
+
+					<!-- Total Highest IVSum -->
+					<div
+						class="bg-light-primary dark:bg-dark-primary rounded-md grid grid-flow-col p-2 gap-2"
+					>
+						<div class="grid grid-flow-row text-left">
+							<span>Total Highest IV Pokemon</span>
+							<span class="font-bold text-sm self-end text-light-text-placeholder dark:text-dark-text-placeholder">{{
+								data.totals.highest_iv_sum_pokemon
+							}}</span>
+						</div>
+						<div class="grid grid-flow-row text-right">
+							<span>Total Highest IVSum</span>
+							<span class="font-bold text-sm self-end text-light-text-placeholder dark:text-dark-text-placeholder">{{
+								data.totals.highest_iv_sum
+							}}</span>
+						</div>
+					</div>
+
+					<!-- Total Lowest IVSum -->
+					<div
+						class="bg-light-primary dark:bg-dark-primary rounded-md grid grid-flow-col p-2 gap-2"
+					>
+						<div class="grid grid-flow-row text-left">
+							<span>Total Lowest IV Pokemon</span>
+							<span class="font-bold text-sm self-end text-light-text-placeholder dark:text-dark-text-placeholder">{{
+								data.totals.lowest_iv_sum_pokemon
+							}}</span>
+						</div>
+						<div class="grid grid-flow-row text-right">
+							<span>Total Lowest IVSums</span>
+							<span class="font-bold text-sm self-end text-light-text-placeholder dark:text-dark-text-placeholder">{{
+								data.totals.lowest_iv_sum
+							}}</span>
+						</div>
+					</div>
+
+					<!-- Phase Lowest IVSum -->
+					<div
+						class="bg-light-primary dark:bg-dark-primary rounded-md grid grid-flow-col p-2 gap-2"
+					>
+						<div class="grid grid-flow-row text-left">
+							<span>Phase Lowest IV Pokemon</span>
+							<span class="font-bold text-sm self-end text-light-text-placeholder dark:text-dark-text-placeholder">{{
+								data.totals.phase_lowest_iv_sum_pokemon
+							}}</span>
+						</div>
+						<div class="grid grid-flow-row text-right">
+							<span>Phase Lowest IVSum</span>
+							<span class="font-bold text-sm self-end text-light-text-placeholder dark:text-dark-text-placeholder">{{
+								data.totals.phase_lowest_iv_sum
+							}}</span>
+						</div>
+					</div>
+
+					<!-- Shiny averages -->
+					<div
+						class="bg-light-primary dark:bg-dark-primary rounded-md grid grid-flow-col p-2 gap-2"
+					>
+						<div class="grid grid-flow-row gap-2">
+							<div class="grid grid-flow-row text-left">
+								<span>Shiny Average</span>
+								<span class="font-bold text-sm self-end text-light-text-placeholder dark:text-dark-text-placeholder">{{
+									data.totals.shiny_average
+								}}</span>
+							</div>
+							<div class="grid grid-flow-row text-left">
+								<span>Shortest Phase Pokemon</span>
+								<span class="font-bold text-sm self-end text-light-text-placeholder dark:text-dark-text-placeholder">{{
+									data.totals.shortest_phase_pokemon
+								}}</span>
+							</div>
+						</div>
+						<div class="grid grid-flow-row gap-2">
+							<div class="grid grid-flow-row text-right">
+								<span>Shiny Encounters</span>
+								<span class="font-bold text-sm self-end text-light-text-placeholder dark:text-dark-text-placeholder">{{
+									data.totals.shiny_encounters
+								}}</span>
+							</div>
+							<div class="grid grid-flow-row text-right">
+								<span>Shortest Phase Encounters</span>
+								<span class="font-bold text-sm self-end text-light-text-placeholder dark:text-dark-text-placeholder">{{
+									data.totals.shortest_phase_encounters
+								}}</span>
+							</div>
+						</div>
+					</div>
+				</div>
+	</div>
+	</Modal>
+</template>
