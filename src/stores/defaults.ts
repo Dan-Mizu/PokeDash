7a0bca95137b46fef79885744888e773004044ee
Dan <dan@danmizu.dev>
2023-11-05 03:52:25 -0800

    Instance References Object Array
    - Converted Instance References from a string array consisting of just the API URL to an object that can contain way more info such as endpoint fetch times, names, and a reference to the instance data index to support re-ordering in the future.

--- src/stores/defaults.ts
+++ src/stores/defaults.ts
@@ -3,7 +3,7 @@ export default {
 	instances: [] as string[],
 
 	// cached instance data
-	instanceData: [] as IInstanceData[],
+	instanceData: [] as TInstanceData,
 
 	// cached pokemon sprites
 	pokemonSprites: {} as { [key: string]: string },
