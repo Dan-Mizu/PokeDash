70117569a5c3f99691ff9162228a61d406073c3b
Dan <dan@danmizu.dev>
2023-11-05 01:04:25 -0700

    implemented chartjs w example

---
+++ src/pages/chart.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { Bar } from "vue-chartjs";
+const chartData = ref({
+	labels: ["January", "February", "March", "April", "May"],
+	datasets: [
+		{
+			label: "Data One",
+			backgroundColor: "#f87979",
+			data: [40, 20, 12, 50, 10],
+		},
+	],
+});
+const chartOptions = ref({
+	responsive: true,
+	maintainAspectRatio: false,
+});
+</script>
+<template>
+	<div>
+		<Bar :data="chartData" :options="chartOptions" />
+	</div>
+</template>
---
+++ src/plugins/chartjs.ts
@@ -0,0 +1,19 @@
+import {
+	Chart,
+	Title,
+	Tooltip,
+	Legend,
+	BarElement,
+	CategoryScale,
+	LinearScale,
+} from "chart.js";
+export default defineNuxtPlugin(() => {
+	Chart.register(
+		CategoryScale,
+		LinearScale,
+		BarElement,
+		Title,
+		Tooltip,
+		Legend
+	);
+});
