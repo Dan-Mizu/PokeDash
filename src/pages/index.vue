04fe02881b87b4a07858648d958369a7357a4763
U2EZNeko <nobody.cares@gmx.net>
2023-11-13 21:10:58 +0100

    Added refresh timer for all data.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -13,58 +13,61 @@ const store = useStore();
 // initial active view
 const activeView: ShallowRef<Component> = shallowRef(Loading);
 
+const fetchData = async (apiURL: string, dataIndex: number) => {
+  try {
+    const response: IInstanceData = await store.fetchAllInstanceEndpointData(apiURL);
+
+    // save response for this endpoint on this instance
+    store.instanceData[dataIndex] = response;
+
+    // update lastFetched value
+    store.instances[dataIndex].lastFetched = Date.now();
+
+    // update lastFetchedEndpoint value
+    store.instances[dataIndex].lastFetchedEndpoint = {
+      trainer: undefined,
+      items: undefined,
+      party: undefined,
+      encounter_log: undefined,
+      shiny_log: undefined,
+      encounter_rate: undefined,
+      stats: undefined,
+      emulator: undefined,
+    };
+
+    for (const endpoint in response) {
+      (store.instances[dataIndex].lastFetchedEndpoint as CreateMutable<TInstanceEndpointLastFetched>)[endpoint as TInstanceEndpoint] = Date.now();
+    }
+  } catch (error) {
+    console.error(`Error fetching data for ${apiURL}`, error);
+  }
+};
+
 onMounted(async () => {
-	// no longer loading
-	activeView.value = InstancePanels;
-});
+  // no longer loading
+  activeView.value = InstancePanels;
 
-// update instance data for all stored instances
-store.instances.forEach(async (_data, index) => {
-	// fetch all endpoints from each instance's api
-	await store
-		.fetchAllInstanceEndpointData(store.instances[index].apiURL)
-		.then((response: IInstanceData) => {
-			// save response for this endpoint on this instance
-			store.instanceData[store.instances[index].dataIndex] = response;
-
-			// update lastFetched value
-			store.instances[index].lastFetched = Date.now();
-
-			// update lastFetchedEndpoint value
-			store.instances[index].lastFetchedEndpoint = {
-				trainer: undefined,
-				items: undefined,
-				party: undefined,
-				encounter_log: undefined,
-				shiny_log: undefined,
-				encounter_rate: undefined,
-				stats: undefined,
-				emulator: undefined,
-			};
-			for (const endpoint in store.instanceData[
-				store.instances[index].dataIndex
-			] as IInstanceData)
-				(
-					store.instances[index]
-						.lastFetchedEndpoint as CreateMutable<TInstanceEndpointLastFetched>
-				)[endpoint as TInstanceEndpoint] = Date.now();
-		});
+  // Set up periodic data fetching for each instance
+  store.instances.forEach((_data, index) => {
+    const { apiURL, dataIndex } = store.instances[index];
+    setInterval(() => fetchData(apiURL, dataIndex), 5000); // Fetch data every 5 seconds
+  });
 });
 
 // determine props needed for active view
 const activeProps = computed(() => {
-	if (activeView.value == InstancePanels) {
-		return {
-			instances: store.instances,
-			instanceData: store.instanceData,
-		};
-	}
+  if (activeView.value == InstancePanels) {
+    return {
+      instances: store.instances,
+      instanceData: store.instanceData,
+    };
+  }
 });
 </script>
 
 <template>
-	<div class="h-screen overflow-hidden flex justify-center">
-		<!-- View -->
-		<component :is="activeView" v-bind="activeProps" />
-	</div>
+  <div class="h-screen overflow-hidden flex justify-center">
+    <!-- View -->
+    <component :is="activeView" v-bind="activeProps" />
+  </div>
 </template>
